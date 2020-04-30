4c394fdaaed6ec7811b92f1810b508796e4e4b83
Eltotino <totojaber1@gmail.com>
2020-04-30 15:49:02 +0100

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
 arglist = sys.argv
 num = len(arglist)
 j = 0
@@ -9,6 +8,5 @@ if num == 1:
     print("0")
 else:
     for i in range(1, num):
-    	
-    	j = j + int(arglist[i])
+        j = j + int(arglist[i])
     print("{}".format((j)))
