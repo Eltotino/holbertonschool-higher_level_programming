729e4c316eab09fd277d70f46afaa7bce8a1c67b
Eltotino <totojaber1@gmail.com>
2020-04-30 15:47:21 +0100

    infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+arglist = sys.argv
+num = len(arglist)
+j = 0
+if num == 1:
+    print("0")
+else:
+    for i in range(1, num):
+    	
+    	j = j + int(arglist[i])
+    print("{}".format((j)))
