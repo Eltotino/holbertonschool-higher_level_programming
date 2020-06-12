69c92ed1dcd1b5b06915203e9e8daacbeaa02f87
Eltotino <totojaber1@gmail.com>
2020-06-12 10:16:19 +0100

    multiple_returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def multiple_returns(sentence):
+    first = sentence[0]
+    length = len(sentence)
+    t = (length, first)
+    if length == 0:
+        first = None
+    else:
+        return t
