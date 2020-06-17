ead107721c35edd4beac9a7a47adf4e065c49f72
Eltotino <totojaber1@gmail.com>
2020-06-17 13:39:28 +0100

    complexe

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,5 +3,4 @@ def complex_delete(a_dictionary, value):
     for k in list(a_dictionary.keys()):
        if a_dictionary[k] == value:
         del a_dictionary[k]
-        print (a_dictionary)
     return a_dictionary
