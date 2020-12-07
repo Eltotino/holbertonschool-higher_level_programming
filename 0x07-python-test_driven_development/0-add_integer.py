53e9a61cbce9364fe4e7c9ec196365ca2d584afe
Eltotino <totojaber1@gmail.com>
2020-12-07 02:31:47 +0100

    tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Add_integer module
+"""
+
+def add_integer(a, b=98):
+	"""
+	Addition of two integer a and b 
+
+	Args:
+	a : integer
+	b : integer
+
+	Raises:
+	TypeError: if args are not integer
+
+
+	Returns:
+	(int) : addition of a  and b
+	"""
+	if type(a) not in [int, float]:
+		raise TypeError("a must be an integer")
+	if type(b) not in [int, float]:
+		raise TypeError("b must be an integer")
+	if (a + b) == float('inf') or (a + b) == -float('inf'):
+		return int( a+ b)
+	return int(a) + int(b)
