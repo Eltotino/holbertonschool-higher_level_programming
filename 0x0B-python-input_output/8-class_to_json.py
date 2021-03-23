9620b0ee078e81934137dbeb297a3739cddef183
Eltotino <totojaber1@gmail.com>
2021-03-23 20:23:03 +0100

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class to json module"""
+
+
+def class_to_json(obj):
+    """Gives a dictionary description
+    with simple data structure
+
+    Args:
+        obj(obj): given object
+
+    Returns:
+        a dictionary with simple data structure
+    """
+    return obj.__dict__
