08c049759be6edba3003dd71a89be4c21aa7b653
Eltotino <totojaber1@gmail.com>
2021-03-23 12:54:57 +0100

    square2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Square module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class from Rectangle
+
+    Attibutes:
+        size (int): size of the square
+    """
+    def __init__(self, size):
+        """Initialises the square
+
+        Args:
+            size(int): size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
