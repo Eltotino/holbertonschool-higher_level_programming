faafefe8903fcae97d77a8acebe916aff81e6964
Eltotino <totojaber1@gmail.com>
2021-03-23 11:18:01 +0100

    rrectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle inherits from BaseGeometry Class
+
+    Attributes:
+        width (int): width of the rectangle
+        height (int):height of the rectangle
+    """
+    def __init__(self, width, height):
+        """ Initialises the rectangle
+
+        Args:
+            width (int): width of the rectangle
+            height (int):height of the rectangle
+            """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
