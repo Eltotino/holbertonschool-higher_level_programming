29c2d7eadce2bc1041b3eff5e4df498300fb7768
Eltotino <totojaber1@gmail.com>
2021-03-23 10:36:23 +0100

    inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -14,7 +14,7 @@ def inherits_from(obj, a_class):
         True if is a subclass
         False if otherwise
     """
-    if isinstance(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
-         return False
+        return False
