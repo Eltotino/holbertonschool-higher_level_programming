95fb48ac3b4a71f36097fdaed5e8dec9ed6b9a14
Eltotino <totojaber1@gmail.com>
2021-03-23 17:20:18 +0100

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write File module"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string into a file and 
+    returns number of lines in a txt file
+
+    Args:
+        filename(str): file name
+        text(str): a given text
+    """
+    with open(filename, mode='w', encoding="UTF-8") as my_file:
+        my_file.write(text)
+
+    with open(filename, encoding="UTF-8") as my_file:
+
+        count = 0
+        for word in my_file:
+            for char in word:
+                count += 1
+
+    return count
