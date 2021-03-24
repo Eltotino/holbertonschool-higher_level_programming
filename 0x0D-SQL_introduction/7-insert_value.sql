ef46bd03d83aac913431d71e09a99e62ff38db38
Eltotino <totojaber1@gmail.com>
2021-03-24 17:58:27 +0100

    insert

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Script that inserts new row in the table first_table from hbtn_0c_0 in MySQL server
+-- Inseerts new row in first_table in Mysql server
+INSERT INTO first_table(id, name) VALUES(89, 'Holberton School');
