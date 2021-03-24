225a84ca9cb48e016e560e516e4620dc7eca8d62
Eltotino <totojaber1@gmail.com>
2021-03-24 18:25:29 +0100

    top_score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Script that lists all records of the table second_table from hbtn_0c_0 in MySQL server
+-- Lists all records from second_table ordered by score in descending order
+SELECT score, name FROM second_table
+ORDER BY score DESC;
