0c7ba46d272aaa321daa52976870d27d45c4aa46
Eltotino <totojaber1@gmail.com>
2021-03-24 17:43:34 +0100

    full_table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Script that prints full description of the tabel first_table from hbtn_0c_0 in MySQL server
+-- Prints full description of first_table first table in Mysql server
+SHOW CREATE TABLE first_table
