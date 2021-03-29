bd97c9cafc092fb14b052357f5ea7fef317bfd87
Eltotino <totojaber1@gmail.com>
2021-03-29 23:35:04 +0100

    loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const loop = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let index = 0; index < loop.length; index++) {
+  const sentence = loop[index];
+  console.log(sentence);
+}
