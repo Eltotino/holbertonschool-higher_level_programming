18713a352e43eb4e4c0d2497e28810354f1ae72a
Eltotino <totojaber1@gmail.com>
2021-03-29 22:29:48 +0100

    muti

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const Line1 = 'C is fun'
-const Line2 = 'Python is cool'
+const Line1 = 'C is fun';
+const Line2 = 'Python is cool';
 const myVar = 'JavaScript is amazing';
 
-console.log((Line1),'\n',(Line2),'\n',(myVar))
-
+console.log(Line1);
+console.log(Line2);
+console.log(myVar);
