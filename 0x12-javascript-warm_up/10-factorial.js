c7e98cae5409d812c53bf0525f6644af55748a08
Eltotino <totojaber1@gmail.com>
2021-03-30 12:19:45 +0100

    facto

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 function factorial (value) {
   if (!value || value < 2) {
-  	return(1);
+    return (1);
   }
-  return value * factorial(value -1);
+  return value * factorial(value - 1);
 }
 console.log(factorial(Number(process.argv[2])));
