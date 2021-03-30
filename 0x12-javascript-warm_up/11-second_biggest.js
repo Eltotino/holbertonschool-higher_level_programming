164ef743dc5709933114098e9a2e5505678d1f26
Eltotino <totojaber1@gmail.com>
2021-03-30 12:53:15 +0100

    second_big

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const arr = process.argv.slice(2);
 
-const max = arr.sort((a, b) => b - a)[1];
-console.log(max);
-
 if (arr.length <= 2) {
   console.log(0);
+} else {
+  const max = arr.sort((a, b) => b - a)[1];
+  console.log(max);
 }
