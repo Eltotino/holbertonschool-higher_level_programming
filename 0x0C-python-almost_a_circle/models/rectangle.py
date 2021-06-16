3e866bb138784f4d7287559b75b2a9635bd5d12c
Eltotino <totojaber1@gmail.com>
2021-06-16 17:16:08 +0100

    rectangl

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Rectangle module"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class
+
+    Attributes:
+        width (int): rectangle's width
+        height (int): rectangle's height
+        x (int): rectangle's horizontal axis
+        y (int): rectangle's vertical axis
+        id (int) : it's ID
+    """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        Base.__init__(self, id)
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__heigth
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
