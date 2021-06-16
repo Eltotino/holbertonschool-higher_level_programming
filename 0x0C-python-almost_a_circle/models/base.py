9a45683a454fb25dc86ea6571ac5d7cc82034010
Eltotino <totojaber1@gmail.com>
2021-06-16 16:55:23 +0100

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """Base module"""
 
+
 class Base:
     """Base Class"""
     __nb_objects = 0
 
     def __init__(self, id=None):
-    	"""Initialiser of class"""
-    	if id is not None:
-    	    self.id = id
-    	else:
-    	   Base.__nb_objects += 1
-    	   self.id = Base.__nb_objects
+        """Initialiser of class"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
