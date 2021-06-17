c956cb163314bdb10d90f2cb00b64f05a01f06da
Eltotino <totojaber1@gmail.com>
2021-06-17 19:31:55 +0100

    webscrap

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const File = process.argv[2];
 const stream = fs.createReadStream(File, 'utf-8');
 stream.on('error', error => {
-    console.log(error);
+  console.log(error);
 })
   .on('data', data => {
     console.log(data);
