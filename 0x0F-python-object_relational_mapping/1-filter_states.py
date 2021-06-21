a7b5946f6b31b1fe28b310d7feccf870c1c43085
Eltotino <totojaber1@gmail.com>
2021-06-21 14:20:55 +0100

    filter

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Filtering states Module"""
+import MySQLdb
+import sys
+
+
+def select_states():
+    """Filters states from the database"""
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
+
+    cur = db.cursor()
+    cur.execute("SELECT *\
+    	        FROM states\
+    	        WHERE name LIKE BINARY 'N%'\
+    	        ORDER BY id ASC"
+    	        )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+	select_states()
