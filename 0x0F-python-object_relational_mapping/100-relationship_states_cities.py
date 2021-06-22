d601ea9e8f8cc207929f9e55b936f8a3dd314d73
Eltotino <totojaber1@gmail.com>
2021-06-22 19:45:00 +0100

    relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""State Module"""
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from relationship_state import Base, State
+from relationship_city import City
+import sys
+
+
+def create_relationship():
+    """Fetch first state in db"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+             username, password, database), pool_pre_ping=True)
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
+    session.close()
+
+if __name__ == "__main__":
+    create_relationship()
