42f18a08c67a702e91adeb7bfc0b5e149a20d1b5
Eltotino <totojaber1@gmail.com>
2021-06-22 19:05:17 +0100

    delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""State Module"""
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+import sys
+
+
+def delete_state():
+    """Fetch first state in db"""
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+             username, password, database), pool_pre_ping=True)
+
+    Session = sessionmaker()
+    session = Session(bind=engine)
+    for row in session.query(State).filter(State.name.like('%a')):
+        session.delete(row)
+    session.commit()
+    session.close()
+
+if __name__ == "__main__":
+    delete_state()
