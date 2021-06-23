d59375b7f540e548b2456b846c7ed8094b41f284
Eltotino <totojaber1@gmail.com>
2021-06-23 15:54:27 +0100

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as path:
         body = path.read()
     print("Body response:")
-    print("\t- type: {}".format(body))
+    print("\t- type: {}".format(type(body)))
     print("\t- content: {}".format(body))
     print("\t- utf-8 content: {}".format(body))
