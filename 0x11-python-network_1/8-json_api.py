e6c46825211b6d4d1a7ebb3940b4cbdba3308a2e
Eltotino <totojaber1@gmail.com>
2021-06-23 17:18:00 +0100

    api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,9 +5,7 @@ import sys
 
 if __name__ == '__main__':
     data = {"q": sys.argv[1] if len(sys.argv) > 1 else ""}
-    r = requests.post(
-        ' http://cb9651e5eb21.ea891bec.hbtn-cod.io:5000/search_user', data=data
-        )
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
     try:
         if not r.json():
             print("No result")
