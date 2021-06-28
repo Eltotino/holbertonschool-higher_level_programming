aa29258f1a7cc3710062a170dfc0309799954af2
Eltotino <totojaber1@gmail.com>
2021-06-28 16:10:37 +0100

    persos

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const Film = process.argv[2];
-const apiUrl = 'https://swapi-api.hbtn.io/api';
+const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
 request(apiUrl + Film, (error, response, body) => {
   if (!error) {
     const persos = JSON.parse(body).persos;
