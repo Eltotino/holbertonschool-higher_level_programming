0cf9f838801e954cc3b87936ac3951848fb8a992
Eltotino <totojaber1@gmail.com>
2021-06-28 15:57:39 +0100

    todos

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+request(apiUrl, (error, response, body) => {
+  if (!error) {
+    const all = JSON.parse(body);
+    const result = {};
+    for (const alls of todos) {
+      if (alls.completed) {
+        if (!result[alls.UserId]) {
+          result[alls.userId] = 1;
+        } else {
+          result[alls.userId]++;
+        }
+      }
+    }
+    console.log(result);
+  }
+});
