799a624b6b7941e8a986e85db5df0efca56eca3b
Eltotino <totojaber1@gmail.com>
2021-06-28 16:08:21 +0100

    persos

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const Film = process.argv[2];
+const apiUrl = 'https://swapi-api.hbtn.io/api';
+request(apiUrl + Film, (error, response, body) => {
+  if (!error) {
+    const persos = JSON.parse(body).characters;
+    for (const perso of persos) {
+      request(perso, (error, response, body) => {
+        if (!error) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
