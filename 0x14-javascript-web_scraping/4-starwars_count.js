2f7cba4768519363eec7510e8c13c024b68fe447
Eltotino <totojaber1@gmail.com>
2021-06-28 15:46:39 +0100

    18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,9 @@ const request = require('request');
 const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (!error) {
-    const result = JSON.parse(body).result;
+    const results = JSON.parse(body).results;
     let count = 0;
-    for (const movie of result) {
+    for (const movie of results) {
       if (movie.characters.find(value => value.endsWith('18/'))) {
         count++;
       }
