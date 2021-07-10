ac80bf04bd3e11c50a91f5eb6c7632c108ca4c69
Eltotino <totojaber1@gmail.com>
2021-07-10 18:24:33 +0100

    genre

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows.
+-- tv shows in hbtn_0d_tvshows database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows,
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
