9481170cccb437bdd175cd79c9ce3eb96d40d3a5
Eltotino <totojaber1@gmail.com>
2021-07-10 20:03:40 +0100

    ratings

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,8 +1,8 @@
 -- Queries in MySQL server
 -- Select rating by shows
-SELECT sh.title, sum(r.rate) AS rating
+SELECT sh.title, sum(ra.rate) AS rating
 FROM tv_shows AS sh
-INNER JOIN tv_show_ratings AS r
-ON sh.id = r.show_id
+INNER JOIN tv_show_ratings AS ra
+ON sh.id = ra.show_id
 GROUP BY sh.title
 ORDER BY rating DESC;
