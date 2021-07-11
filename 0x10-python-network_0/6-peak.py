314e30d0421d998ca58a573be94869919b5c7edd
Eltotino <totojaber1@gmail.com>
2021-07-11 19:18:48 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,9 +8,10 @@ def find_peak(list_of_integers):
         return None
     arr = list_of_integers
     peak = arr[0]
-    for i in range(len(arr)):
+    for i in range(0, len(arr) - 1):
         if arr[i+1] > arr[i]:
             peak = arr[i + 1]
         if arr[i - 1] < arr[i - 2]:
             peak = arr[i - 2]
         return peak
+
