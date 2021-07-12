75910d54912ebd0f9abdb69ee7074deb539477d3
Eltotino <totojaber1@gmail.com>
2021-07-12 13:38:20 +0100

    github commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order to display last 10 commits"""
+
+if __name__ == '__main__':
+    import sys
+    import requests
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    info = owner + "/" + repo
+    r = requests.get("https://api.github.com/repos/" + info + "/commits")
+    cmt = r.json()[:10]
+    for i in cmt:
+        elem = i['sha']
+        author = i['commit']['author']['name']
+        print('{}: {}'.format(elem, author))
