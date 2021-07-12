5972c0d86f6a03f41861b35716276d79e1ccfe8b
Eltotino <totojaber1@gmail.com>
2021-07-12 16:42:14 +0100

    star

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,10 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const apiUrl = 'https://swapi-api.hbtn.io/api/films/';
-const Film = process.argv[2];
-request(apiUrl + Film, (error, response, body) => {
-  if (!error) {
-    const persos = JSON.parse(body).characters;
-    console.log(persos);
+const movieId = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let chars;
+const ordNames = {};
+request.get(url, (error, respponse, body) => {
+  (error) ? console.log(error) : chars = JSON.parse(body).characters;
+  for (const id of chars) {
+    request.get(id, (error, response, body) => {
+      if (!error) {
+        ordNames[id] = JSON.parse(body).name;
+        if (Object.values(ordNames).length === chars.length) {
+          for (const id of chars) {
+            console.log(ordNames[id]);
+          }
+        }
+      }
+    });
   }
 });
